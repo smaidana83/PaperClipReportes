78dae01537b244b27e3354f6476bd3ee2fb0ebfa
smaidana83 <smaidana83@gmail.com>
2016-02-03 20:09:39 -0300

    prueba

--- src/com/example/paperclipreportes/PaperclipreportesUI.java
+++ src/com/example/paperclipreportes/PaperclipreportesUI.java
@@ -30,7 +30,7 @@ public class PaperclipreportesUI extends UI {
 		Button button = new Button("Click Me");
 		button.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
+				layout.addComponent(new Label("Thank you for clicking TEST"));
 			}
 		});
 		layout.addComponent(button);
